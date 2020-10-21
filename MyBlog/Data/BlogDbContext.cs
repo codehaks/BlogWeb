78c3c55cb6459f212894e746c7d8ef8f4d1a4106
Hakim Ghods <ghods.hakim@hotmail.com>
2020-10-21 11:42:32 +0330

    Create Table

--- MyBlog/Data/BlogDbContext.cs
+++ MyBlog/Data/BlogDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,7 @@ namespace MyBlog.Data
             optionsBuilder.UseSqlite("Data source=myblog.sqlite");
             base.OnConfiguring(optionsBuilder);
         }
+
+        public DbSet<Blog> Blogs { get; set; }
     }
 }
