eb008ef6cb585f1e7e525e80252818d100703cad
Hakim Ghods <ghods.hakim@hotmail.com>
2020-10-21 11:55:54 +0330

    Create

--- MyBlog/Pages/Blogs/Create.cshtml.cs
+++ MyBlog/Pages/Blogs/Create.cshtml.cs
@@ -4,16 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyBlog.Data;
 using MyBlog.Models;
 
 namespace MyBlog.Pages.Blogs
 {
     public class CreateModel : PageModel
     {
+        private readonly BlogDbContext _db;
+
+        public CreateModel(BlogDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
         public Blog Blog { get; set; }
+
         public IActionResult OnPost()
         {
-            // Save to database
+            _db.Blogs.Add(Blog);
+            _db.SaveChanges();
+
             return Redirect("/index");
         }
     }
