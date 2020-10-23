01a3dcfb1532adfbf85db511cb7858f21ec96f72
Hakim Ghods <ghods.hakim@hotmail.com>
2020-10-23 16:54:57 +0330

    Edit

---
+++ MyBlog/Pages/Blogs/Edit.cshtml.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyBlog.Data;
+using MyBlog.Models;
+
+namespace MyBlog.Pages.Blogs
+{
+    public class EditModel : PageModel
+    {
+        private readonly BlogDbContext _db;
+
+        public EditModel(BlogDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Blog Blog { get; set; }
+
+        public void OnGet(int id)
+        {
+            Blog = _db.Blogs.Find(id);
+        }
+
+        public IActionResult OnPost()
+        {
+            var blog = _db.Blogs.Find(Blog.Id);
+
+            blog.Name = Blog.Name;
+            blog.Description = Blog.Description;
+
+            _db.SaveChanges();
+
+            return RedirectToPage("index");
+        }
+    }
+}
