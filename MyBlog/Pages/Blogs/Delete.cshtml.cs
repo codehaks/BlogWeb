ed72ec7569b6e0f3c3fa6d701ee9751410cabc00
Hakim Ghods <ghods.hakim@hotmail.com>
2020-10-24 10:22:12 +0330

    Delete

---
+++ MyBlog/Pages/Blogs/Delete.cshtml.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyBlog.Data;
+using MyBlog.Models;
+
+namespace MyBlog.Pages.Blogs
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly BlogDbContext _db;
+
+        public DeleteModel(BlogDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Blog Blog { get; set; }
+
+        public void OnGet(int id)
+        {
+            Blog = _db.Blogs.Find(id);
+        }
+
+        public IActionResult OnPost()
+        {
+            var blog = _db.Blogs.Find(Blog.Id);
+
+            _db.Blogs.Remove(blog);
+
+            _db.SaveChanges();
+
+            return RedirectToPage("index");
+        }
+    }
+}
