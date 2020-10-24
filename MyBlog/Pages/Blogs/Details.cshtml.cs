acd1c8c4029437e45730864b91d59235d177015b
Hakim Ghods <ghods.hakim@hotmail.com>
2020-10-24 10:29:46 +0330

    Details

---
+++ MyBlog/Pages/Blogs/Details.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyBlog.Data;
+using MyBlog.Models;
+
+namespace MyBlog.Pages.Blogs
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly BlogDbContext _db;
+
+        public DetailsModel(BlogDbContext db)
+        {
+            _db = db;
+        }
+
+        public Blog Blog { get; set; }
+        public void OnGet(int id)
+        {
+            Blog=_db.Blogs.Find(id);
+            
+        }
+    }
+}
