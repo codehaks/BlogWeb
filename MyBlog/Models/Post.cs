66e318e2c1438c84060a80e9f0787217dd9ee8d2
Hakim Ghods <ghods.hakim@hotmail.com>
2020-11-15 10:31:47 +0330

    fix

--- MyBlog/Models/Post.cs
+++ MyBlog/Models/Post.cs
@@ -5,20 +5,10 @@ namespace MyBlog.Models
 {
     public class Post
     {
-        public long Id { get; set; } //Composite
+        public long Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-
-        public int DaysAgo
-        {
-            get
-            {
-                return (DateTime.Now - TimeCreated).Days;
-            }
-        }
-
-        public int Rating { get; set; }
         public DateTime TimeCreated { get; set; }
-        public DateTime TimeInsert { get; set; }
+
     }
 }
